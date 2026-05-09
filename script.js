5f1e6580cedfc9a3184ae0b1b5358ac26acc835d
EddieT83 <dot.edwardtuttle@gmail.com>
2026-05-08 20:43:53 -0400

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function handleSubmit(event) {
-  event.preventDefault();
-  const status = document.getElementById('form-status');
-  status.textContent = 'Thanks! Your message has been received. We will reply within one business day.';
-  event.target.reset();
-}
